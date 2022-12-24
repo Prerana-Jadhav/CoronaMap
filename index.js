8edc2dae1cbe1df794127018e9605e62aa087a96
Prerana Jadhav <105961146+Prerana-Jadhav@users.noreply.github.com>
2022-12-24 13:10:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function updateMap() {
+    console.log("Updating with real-time data");
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                const cases=element.infected;
+                if(cases>255){
+                    color="rgb(255,0,0)"
+                }else{
+                    color=`rgb(${cases},0,0)`
+                }
+
+                //Mark on the map
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+
+            // Marker hover function
+            
+        })
+}
+
+let interval=2000;
+setInterval(updateMap,interval);
